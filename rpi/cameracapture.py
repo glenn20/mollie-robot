3b2b541026be348819cf75c84e221907bae5d495
Glenn Moloney <glenn.moloney@gmail.com>
2015-09-27 23:20:05 +1000

    New modules: cameracapture and workflow workflow creates pools of wrker threads to process items of queues cameracapture simplifies and isolates the robot's image capture from the RPi camera

---
+++ rpi/cameracapture.py
@@ -0,0 +1,100 @@
+import time
+import threading
+
+import imageprocessor
+
+class CameraCapture( theading.Thread ):
+    """
+    Setup the picamera image capture process in a new thread. Images will be captured
+    to io.BytesIO streams in Image objects. After capture, Images will be put onto an
+    outputqueue ready for further processing....
+
+    Methods:
+    run():              Run the continuous image capture process
+    close():            Close down the threads and print a report()
+    _streamgenerator(): Generator function to yield the next stream for image capture
+    """
+
+    def __init__( self,
+                  camera,
+                  inputqueue,
+                  outputqueue,
+                  showpreview = False ):
+        """
+        Constructor for multi-threaded image capture and processing.
+
+        Args:
+            camera (picamera.PiCamera): A pre-configured Raspberry PiCamera object
+            inputqueue (Queue.Queue):   Input queue for loading Image objects
+            outputqueue (Queue.Queue):  Output queue for Image objects after capture
+            showpreview (boolean):      Display image capture preview on RPi console?
+        """
+        super( CameraCapture, self ).__init__()  # Initialise the super class
+        self.camera         = camera
+        self.inputqueue     = inputqueue
+        self.outputqueue    = outputqueue
+        self.showpreview    = showpreview
+        self.done           = False            # Flag to shutdown processing
+        # Fill the inputqueue Image records - ready for capture
+        for i in range(10):
+            self.inputqueue.put( Image() )
+        # Call the "run()" method in the new thread
+        self.start()
+
+    # Setup the camera and run the image capture process
+    def run( self ):
+        """
+        Run the image capture and processing and robot control loop.
+
+        Setups up and executes the image capture and processing.
+        """
+        with self.camera as camera:
+            if (self.showpreview):
+                camera.start_preview()
+            # time.sleep(2)
+            # This is the fastest method to capture a sequence of images from the camera
+            # Keep capturing images until self.done is True...
+            camera.capture_sequence( self._streamgenerator(), use_video_port=True )
+
+    def close( self ):
+        # Stop the image capture
+        self.done = True
+        # self.join()
+
+    def _streamgenerator( self ):
+        """
+        Generator to yield the streams for image capture and acquisition.
+
+        Yields the next stream object from the input queue.
+        Intended to be used as the stream generator for
+        PiCamera.capture_sequence().
+
+        Arguments:
+            None
+        Yields:
+            stream (BytesIO())
+        """
+        # Get the next Image from the input queue
+        # Yield the Image object's stream to the picamera capture-sequence
+        # When capture is complete - put the Image on the output queue
+        while not self.done:
+            try:
+                # Get the next Image buffer from the camera queue
+                # We use a timeout so we can respond to (self.done=True)
+                image = self.inputqueue.get( timeout=0.2 )
+            except Queue.Empty:
+                continue
+            if image is not None:
+                image.reset()
+                image.time = time.time() # Record the time for the capture
+                yield image.stream
+                # Resumes when camera asks for next buffer for Image capture
+                # Put the just captured Image on the processing queue
+                image.stream.seek( 0 )
+                self.outputqueue.put( image )
+
+# Local Variables:
+# python-indent: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# End: 
---
+++ rpi/workflow.py
@@ -0,0 +1,157 @@
+import io
+import time
+import threading
+import Queue
+
+class Worker( threading.Thread ):
+    """
+    Hold information for a worker thread
+    """
+    sentinel = object()
+
+    def ___init__( self, processor, inputqueue, outputqueue, otherqueues = None ):
+        super( Worker, self ).__init__()
+        self.processor      = processor
+        self.inputqueue     = inputqueue
+        self.outputqueue    = outputqueue
+        self.otherqueues    = otherqueues
+        self.count          = 0
+        self.start()
+
+    def run( self ):
+        """
+        Extract items off input queue, process, and then put on the output queue.
+        Args:
+            inputqueue:   Queue.Queue
+            outputqueues: A list/tuple of Queue.Queue-s
+            processor:    Function to do the work - process the items.
+        """
+        while True:
+           # Wait for an item to appear on the processing queue
+            item = self.inputqueue.get()
+            # If this is a sentinel - put it back on the queue and end this worker thread.
+            #  - need to put it back so other threads will also find it and terminate.
+            if (item is self.sentinel):
+                self.inputqueue.put( self.sentinel )
+                # Also pass the sentinel down the processing pipeline
+                if (self.outputqueue is not None):
+                    self.outputqueue.put( self.sentinel )
+                break
+            try:
+                # Do the processing
+                if self.otherqueues is not None:
+                    item = self.processor( item, self.otherqueues )
+                else:
+                    item = self.processor( item )
+                self.count += 1
+            finally:
+                # Put the item on the output queue
+                if (self.outputqueue is not None):
+                    self.outputqueue.put( item )
+
+    def close( self ):
+        # Put a sentinel on the end of the work queue
+        # - Worker thread will close when it is received.
+        self.inputqueue.put( self.sentinel )
+
+class WorkerPool():
+    """
+    A pool of threads to process items off a queue...
+    """
+    def __init__( self, numberofworkers, processor, inputqueue, outputqueue, otherqueues = None ):
+        """
+        Args:
+        - numberofworkers:  The number of worker threads to create.
+        - processor:        The function to process items: first arg is the item to process
+        -                   An optional second arg which is additional queues for other output
+        - inputqueue:       The queue for receiving items for processing
+        - outputqueue:      The queue to put processed items on when finished
+        - otherqueues:      An optional list/tuple of queues to be passed to the processor function
+        -                   The procesor function may use these to run new workflows
+        """
+        self.numberofworkers = numberofworkers
+        self.processor       = processor
+        self.inputqueue      = inputqueue
+        self.outputqueue     = outputqueue
+        self.otherqueues     = otherqueues
+        self.workers         = []
+
+        # Now create the Worker threads
+        self.addworkers( numberofworkers )
+
+    def addworkers( self, numberofworkers ):
+        """
+        Add more worker threads to this pool.
+        Args:
+        - numberofworkers: The number of worker threads to add.
+        """
+        self.workers.extend( [ Workflow( self.processor, self.inputqueue,
+                                         self.outputqueue, self.otherqueues )
+                               for i in range( numberofworkers ) ] )
+
+    def count( self ):
+        """
+        Get the number of items processed by all the workers in this pool.
+        """
+        return sum( (worker.count for worker in self.workers) )
+
+    def close( self ):
+        """
+        Flag all the worker threads to finish processing the queues and close down.
+        """
+        for worker in self.workers:
+            worker.close()
+        for worker in self.workers:
+            worker.join()
+
+class WorkflowManager():
+    """
+    Manage the queues and threads for workflow.
+    """
+    def __init__( self, workflow ):
+        """
+        Takes a workflow description and builds and initiates tasks to process the workflow
+        Args:
+        workflow: A list/tuple of WorkerPool objects which comprise the workflow
+        """
+        self.starttime  = time.time()
+        self.finishtime = None
+        self.workflow   = workflow
+
+    def self.count():
+        # Return the number of items processed
+        # - only count the items processed by the first pool of workers.
+        return self.workflow[0].count() if self.workflow is not None else 0
+
+    def report( self ):
+        """
+        Print a summary of the workflow statistics.
+        """
+        elapsedtime = self.finishtime - self.starttime
+        print( "\r\n" )
+        print( 'Processed %d items in %d seconds at %.2ffps\r\n'
+               % ( self.count(), elapsedtime, self.count / elapsedtime ) )
+
+    # Cleanup up all the worker threads
+    def close( self ):
+        """
+        Close down the workflow processing threads.
+        """
+        # Tell the worker threads to finish and wait for them to terminate...
+        for workerpool in self.workflow:
+            workerpool.close()
+        self.finishtime = time.time()
+        self.report()
+
+    # So we can use this class as a ContextManager
+    def __enter__( self ):
+        return self
+
+    def __exit__( self, exc_type, exc_val, exc_tb ):
+        self.close()
+
+# Local Variables:
+# python-indent: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# End: 
